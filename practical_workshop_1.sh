edd0fb4291799a7a037a11251921e39df4f11425
Mateito10028 <canarpedro6@gmail.com>
2026-03-30 12:56:05 +0000

    one

---
+++ practical_workshop_1.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt apt upgrade
+sudo apt install parted
+#sudo: Executes commands with superuser or system administrator privileges.
+#apt: Package management tool that allows you to install, update, and remove software.
+#update: Updates the local list of available packages and their versions from the repositories.
+#update: Installs the latest versions of all packages already on the system.
+#install: Downloads and installs a specific software package along with itsdependencies.
+#parted: Command-line utility for creating, destroying, and manipulating disk partitions.
