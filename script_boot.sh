06f64e3c793efb8e537cf0a47ecd0bed0c7c4d0b
Mateito10028 <canarpedro6@gmail.com>
2026-04-06 12:41:53 +0000

    four

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,11 @@
 # cd /home/codespace Change the current directory to the user's specific absolute path within the "home" folder.
 # cd ~ Change the current directory to the home directory of the user who is logged in.
 # cd %HOME : Windows command (CMD) that changes the user's home directory using an environment variable.
+
+# mkdir proyecto Create a new folder or directory called "project" in the current location.
+# cd proyecto/ Access or enter the directory called "project" to start working within it.
+# ls -lai It displays a detailed list of all files (including hidden ones), with their permissions, size, and system identifiers (inodes).
+#total 12
+#925541 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:33 .
+#918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:33 ..
+
