8b2efe74ba5400108ba184f9e84a6ba7edda497a
Mateito10028 <canarpedro6@gmail.com>
2026-04-06 13:15:49 +0000

    seven

--- script_boot.sh
+++ script_boot.sh
@@ -15,3 +15,19 @@
  # mkdir -p /tmp/preuba/sub1 /tmp/prueba/sub2 Creates directories recursively, generating parent folders if they do not exist, to establish the "sub1" and "sub2" paths simultaneously.
  # stat /tmp/prueba Displays detailed information and status of a file or directory, including size, permissions, access and creation dates, and system identifiers.
  # man mkdir Open the mkdir command user manual to view its complete documentation, available options, and usage syntax.
+
+# pwd where i am
+# whoami a person whith the sesion connect
+# ls What question is here?
+# ls -l What question is here?
+# ls -la What question is here?
+# ls -lh What question is here?
+
+# ls -lt Sorts the long list of files by modification date, showing the most recent ones first.
+
+# ls / List the contents of the root directory
+
+# ls /etc | head -20 List the files in the configuration folder (/etc) and, using a pipe, display only the first 20 lines.
+# ls /dev | head -20 List the system device files (/dev) and limit the output to the first 20 items found.
+
+# ls -la Displays a detailed list of all files in the current directory, including permissions, owners, and hidden files.
