94543ef10da644a2df19a0815b8a7269195abfc8
Mateito10028 <canarpedro6@gmail.com>
2026-04-06 12:38:15 +0000

    third

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+# cd is whn we need change of the directory
+# cd /home/codespace Change the current directory to the user's specific absolute path within the "home" folder.
+# cd ~ Change the current directory to the home directory of the user who is logged in.
+# cd %HOME : Windows command (CMD) that changes the user's home directory using an environment variable.
