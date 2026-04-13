8b4d498e6b8d87cbd2f81c9c424f9da82e30bfda
Mateito10028 <canarpedro6@gmail.com>
2026-04-13 12:53:14 +0000

    four

--- script.sh
+++ script.sh
@@ -6,3 +6,8 @@ ls -l   Delete a directory only if it is completely empty.
 ls -l -a -h  Borra un directorio y todo su contenido (archivos y subdirectorios) de forma recursiva.
 ls -ah   Borra un directorio de forma recursiva y forzada, ignorando archivos inexistentes y sin solicitar confirmación.
 
+rm -- -rf   Permanently deletes a specific file that is literally called -rf
+rmdir -- -rf  Deletes an empty directory that has the literal name -rf.
+
+
+
