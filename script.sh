aa5ad22a33ccfd5fc4b221c29eada9ccdb382d35
Mateito10028 <canarpedro6@gmail.com>
2026-04-13 12:42:05 +0000

    second

---
+++ script.sh
@@ -0,0 +1,6 @@
+ls -a      : Shows all files and folders in the current directory, including hidden ones (those that begin with a dot).
+ls -all   Displays all files (including hidden ones) in a detailed list format, with information on permissions, size, and date.
+ls -a /  Displays all files and folders in the system's root directory, including hidden items.
+
+
+
