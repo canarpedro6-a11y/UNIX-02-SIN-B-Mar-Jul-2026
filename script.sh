ad36a11a8585a0be7fa0da77f00da91f1b1def0f
Mateito10028 <canarpedro6@gmail.com>
2026-04-13 12:46:01 +0000

    third

--- script.sh
+++ script.sh
@@ -2,5 +2,7 @@ ls -a      : Shows all files and folders in the current directory, including hid
 ls -all   Displays all files (including hidden ones) in a detailed list format, with information on permissions, size, and date.
 ls -a /  Displays all files and folders in the system's root directory, including hidden items.
 
-
+ls -l   Delete a directory only if it is completely empty.
+ls -l -a -h  Borra un directorio y todo su contenido (archivos y subdirectorios) de forma recursiva.
+ls -ah   Borra un directorio de forma recursiva y forzada, ignorando archivos inexistentes y sin solicitar confirmación.
 
