3667627c1a38cd47d9cb8ad5c63746398d4a093d
Mateito10028 <canarpedro6@gmail.com>
2026-05-01 04:18:25 +0000

    four

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -7,4 +7,14 @@ ls -l -r #It displays the files in a detailed list format (permissions, owner, s
 ls -rl #It performs exactly the same function as the previous one, combining the flags to display the long list in reverse order.
 ls -lr #It is an alternative way of writing the same parameters, also resulting in a detailed list ordered from Z to A.
 pwd #Indicates that the user is currently in their home folder
+cd Documents #change to documents directory
+cd / #To navigate to the root directory
+#To return the main
+cd wokspace #change directory
+cd UNIX-02-SIN-B-Mar-Jul-2026 #change directory
+cd .. #It moves you up one level in the folder hierarchy, returning to the parent directory.
+cd ~ #It takes you directly to the current user's home folder, no matter where you are in the system.
+
+
+
 
