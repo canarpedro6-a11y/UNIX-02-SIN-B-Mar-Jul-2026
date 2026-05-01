b5921f8db13565778bfdacf2d53eead873e23b8c
Mateito10028 <canarpedro6@gmail.com>
2026-05-01 04:01:03 +0000

    two.2

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,4 +1,9 @@
 ls #This command is used to monitor directories.
 ls documents #The result is a list of the files included in the Documents directory.
 aptitude moo # command not found
+ls -l #provide more information about each of the listed files
+ls -r #The results will be printed in reverse alphabetical order.
+ls -l -r #It displays the files in a detailed list format (permissions, owner, size) and sorts them in reverse alphabetical order.
+ls -rl #It performs exactly the same function as the previous one, combining the flags to display the long list in reverse order.
+ls -lr #It is an alternative way of writing the same parameters, also resulting in a detailed list ordered from Z to A.
 
