160ac23429ea9be560c2f0f56d2fbc84f649d062
Mateito10028 <canarpedro6@gmail.com>
2026-05-01 04:02:27 +0000

    three

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -6,4 +6,5 @@ ls -r #The results will be printed in reverse alphabetical order.
 ls -l -r #It displays the files in a detailed list format (permissions, owner, size) and sorts them in reverse alphabetical order.
 ls -rl #It performs exactly the same function as the previous one, combining the flags to display the long list in reverse order.
 ls -lr #It is an alternative way of writing the same parameters, also resulting in a detailed list ordered from Z to A.
+pwd #Indicates that the user is currently in their home folder
 
