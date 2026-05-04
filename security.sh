4ded97270bde0528761ecb8b45009dfdc23ee018
Mateito10028 <canarpedro6@gmail.com>
2026-05-04 13:14:46 +0000

    one

---
+++ security.sh
@@ -0,0 +1,2 @@
+id #A command that displays the real and effective user and group IDs of the current user.
+cat /etc/passwd | head -10 #A command pipeline that reads the system's user account database and outputs only the first ten lines of that file.
