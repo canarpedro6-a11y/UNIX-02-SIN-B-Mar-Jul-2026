351e6857f8f58bc6753f85508656e06dc27111ad
Mateito10028 <canarpedro6@gmail.com>
2026-05-19 00:36:15 +0000

    RECUPEACION

--- prueba.sh
+++ prueba.sh
@@ -3,6 +3,7 @@
     3  git checkout -b eval_p2_1_v1 upstream/eval_p2_1_v1
     4  git push -u origin eval_p2_1_v1
     5  clear
+   #PROBLEMA 1
     6  ls
     7  cd nebula
     8  mkdir borradores 
@@ -18,8 +19,9 @@
    18  cd ..
    19  ls
    20  git add .
-   21  git commit -m "fix problem1: correcion de directorios"
+   21  git commit -m "fix problem1: correcion de directorios recuperacion"
    22  clear
+   #PROBLEMA2
    23  cd bitacoras
    24  ls
    25  mv estrellas.bak estrellas.conf
@@ -31,52 +33,23 @@
    31  mv estrella.bak estrella.conf
    32  git add
    33  git add .
-   34  git commit -m "fix problema2: archivos movidos"
+   34  git commit -m "fix problema2: archivos movidos recuperacion"
    35  clear
+   #PROBLEMA 3
    36  ls
    37  cat cosmos.txt
    38  chmod 640 cosmos.txt
    39  ls
    40  ls -l cosmos.txt
    41  git add .
-   42  git commit -m "Fix problema 3: permisos numericos 640 en cosmos.txt"
+   42  git commit -m "Fix problema 3: permisos numericos 640 en cosmos.txt recuperacion"
+   #PROBLEMA 4
    43  clear
    44  ls
    45  chmod u+x galaxia.sh
    46  chmod o-w esterlla.conf
    47  chmod o-w estrella.conf
    48  git add .
-   49  git commit -m "Fix problema 4: permisos simbolicos corregidos"
-   50  clear
-   51  chmod u+s galaxia.sh
-   52  ls -l
-   53  git add .
-   54  git commit -m "Fix problema 5: SUID activado en galaxia.sh"
-   55  git add.
-   56  git add .
-   57  git commit -m "Fix problema 5: SUID activado en galaxia.sh"
-   58  clear
-   59  chmod +t /tmp/nebula_zone
-   60  mkdir -p /tmp/nebula_zone
-   61  chmod 777 /tmp/nebula_zone
-   62  chmod +t /tmp/nebula_zone
-   63  ls -ld /tmp/nebula_zone
-   64  git add .
-   65  git commit -m "Fix problema 6: sticky bit en /tmp/nebula_zone"
-   66  history
-   67  clear
-   68  gpg --batch --generate-key <<EOF
-Key-Type: RSA
-Key-Length: 3072
-Name-Real: aurora
-Name-Email: aurora@nebula.lab
-Expire-Date: 0
-%no-protection
-%commit
-EOF
-
-   69  gpg --encrypt --recipient aurora@nebula.lab nebula/bitacoras/cosmos.txt
-   70  ls nebula/bitacoras/
-   71  git add .
-   72  git commit -m "Fix problema 7: llave GPG generada y cosmos.txt cifrado"
-   73  history
+   49  git commit -m "Fix problema 4: permisos simbolicos corregidos recperacion"
+   50 
+   51 
