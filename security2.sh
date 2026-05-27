8149f6cad58ff2feb293139075939b610056e9e6
Mateito10028 <canarpedro6@gmail.com>
2026-05-27 13:09:47 +0000

    first

---
+++ security2.sh
@@ -0,0 +1,10 @@
+id #Displays the real and effective user and group IDs for the current user.
+id -gn #Prints only the effective primary group name of the current user.
+touch ~/test_grupo_heredado.txt #Creates an empty text file named test_grupo_heredado.txt in the user's home directory
+ls -la ~/test_grupo_heredado.txt #Displays detailed information (including permissions and ownership) for the newly created file in a long-list format, including hidden aspects.
+
+id -gn #Prints the effective primary group name again (likely to check if the group changed after a previous operation).
+touch ~/antes_de_newgrp.txt #Creates a second empty text file named antes_de_newgrp.txt in the user's home directory.
+ls -la ~/antes_de_newgrp.txt #Displays detailed information and file permissions for the second file to compare its group ownership with the first one.
+
+
