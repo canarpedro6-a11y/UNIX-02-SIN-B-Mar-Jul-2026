e654e3ad7d5b3d9a3ec2d43fe8fd32e7fd7f7bcd
Mateito10028 <canarpedro6@gmail.com>
2026-06-01 13:38:36 +0000

    nine

---
+++ blackhatbash3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+book="black hat bash"
+echo "this book´s name is ${book}"
+
+
+[Pedro-Canar] /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 # bash -x blackhatbash3.sh 
++ book='black hat bash'
++ echo 'this book´s name is black hat bash'
+this book´s name is black hat bash
