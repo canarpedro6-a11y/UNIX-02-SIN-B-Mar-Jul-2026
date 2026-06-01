06150246d6edca6f3b315ea83464c4fb2271210b
Mateito10028 <canarpedro6@gmail.com>
2026-06-01 13:17:54 +0000

    seven

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash  #he standard shebang line that instructs the system to execute the script using the Bash shell interpreter.
 #!/bin/bash -x shebang line that enables debugging mode in Bash, printing each command to the terminal before it is executed.
 #bash -r blackhatbash1.sh A commented-out command that, if active, would run the specified script in a restricted Bash shell environment.
+#bash -n blackhatbash1.sh is for show the problems on te scripts
+set -x
 bash --version
 env
 echo ${SHELL}
@@ -10,3 +12,4 @@ echo &{OSTYPE}
 ps -e -f
 ps -ef
 df --human-readable
+set +x
