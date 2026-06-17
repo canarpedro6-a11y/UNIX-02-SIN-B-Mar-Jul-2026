7ef5ddbb2411183e22376f5642237c4dbd81dc42
Mateito10028 <canarpedro6@gmail.com>
2026-06-17 13:05:44 +0000

    newlogs

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -12,3 +12,20 @@ head log.txt
 awk 'NR < 10' log.txt
 
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/Mozilla/Godzilla/g' log.txt
+
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep "Godzilla" newlog.txt
+grep "Mozilla" log.txt
+grep -H "Mozilla" log.txt; grep -H "Godzilla" newlog.txt
+
+sed 's/ //g' log.txt > newlog1.txt
+
+sed '1d' log.txt > newlogd.txt
+
+sed '$d' log.txt > newlogL.txt
+
+sed '$d' newlog1.txt > newlogL.txt
+
+
